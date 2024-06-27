011f22a7bb1c42ccf6aca683cbbf45ac4c0a0359
Subhankar Senapati <subhankarsenapati1999@gmail.com>
2024-06-28 02:35:32 +0530

    Update payload_gen.py

--- payload_gen.py
+++ payload_gen.py
@@ -1,2 +1,2 @@
-s = ' '.join(["https://www.hackthebox.eu/home/machines/writeup/%d" % i for i in range(1,210)])
+s = ' '.join(["https://www.hackthebox.eu/home/machines/writeup/%d" % i for i in range(1,800)])
 print(s)
